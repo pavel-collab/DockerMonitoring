ecba11342d7469d151e109caa1a9045428d49acf
Filippenko Pavel <filippenko.ps@phystech.edu>
2024-12-09 14:46:45 +0300

    debug new features and tables

--- utils.py
+++ utils.py
@@ -3,12 +3,16 @@ def ns2hours(time):
     time = time / 60
     # if time is less than one minute, don't take it into account
     if time < 1:
-        time = "NULL"
-    return round(time, 2)
+        time = None
+    else:
+        time = round(time, 2)
+    return time
 
 def byte2Kb(mem):
     mem = mem / 1024
     # if using memory less than 1 Kb, don't take it into account
     if mem < 1:
-        mem = "NULL"
-    return round(mem, 2)
+        mem = None
+    else:
+        mem = round(mem, 2)
+    return mem
