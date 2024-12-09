df98830b83ed015f50036092da7389c0736c6e26
Andrey Barannikov <barannikov.av@phystech.edu>
2024-12-10 01:16:04 +0300

    Logging added and some exception handling

--- webapp/general/general.py
+++ webapp/general/general.py
@@ -1,8 +1,10 @@
+import datetime
+
 import psycopg as dbm
 
 from flask import Blueprint, render_template, current_app
 
-from utils import groupby_query_rows
+from utils.db import groupby_query_rows
 
 general_bp = Blueprint('general_bp', __name__,
                        template_folder='templates')
@@ -17,9 +19,16 @@ def main_page():
                            MAX(memory_usage), MAX("time") 
                        FROM container_stats""")
         tile_data = cur.fetchone()
+        if not tile_data[0]:
+            tile_data = ('0', '-', '-', datetime.datetime.min)
+        print(tile_data[-1].strftime("%Y-%m-%d %H:%M:%S"))
         # extracting data for plot on main page
         limit = 100
         cur.execute("""SELECT * FROM container_stats ORDER BY "time"
                        DESC LIMIT (%s)""", (limit,))
-        plot_data = groupby_query_rows(0, cur.fetchall())
+        rows = cur.fetchall()
+        if rows:
+            plot_data = groupby_query_rows(0, rows)
+        else:
+            plot_data = {'': [('', '', '', datetime.datetime.min)]}
     return render_template('general/index.html', tile_data=tile_data, plot_data=plot_data.items())
