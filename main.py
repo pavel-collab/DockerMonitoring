a8fe2ba7e74042bf90c666c4f89d27ab3a0485a8
Filippenko Pavel <filippenko.ps@phystech.edu>
2024-12-19 16:54:15 +0300

    split classes to the separate interfaces

--- main.py
+++ main.py
@@ -7,8 +7,9 @@ import logger_config
 
 logger = logging.getLogger(__name__)
 
-from db_connection import DBConnection
+from db_connection import DBConnection, StatsCollector
 from arguments_parsing import parse_connection_arguments
+from utils import start_docker_client
 
 SLEEP_INTERVAL = 10 # sec
 
@@ -24,9 +25,13 @@ def main():
         logger.critical(f'Exception has been caught during creating db connection.', exc_info=True)
         exit(1)
 
+    docker_client = start_docker_client()
+
+    stats_collector = StatsCollector(db_connection, docker_client)
+
     try:
         while True:
-            db_connection.collect_stats()
+            stats_collector.collect_stats()
             time.sleep(SLEEP_INTERVAL)
     except KeyboardInterrupt:
         db_connection.close_db_connection()
--- utils.py
+++ utils.py
@@ -1,18 +1,12 @@
-def ns2hours(time):
-    time = time * 1e-9
-    time = time / 60
-    # if time is less than one minute, don't take it into account
-    if time < 1:
-        time = None
-    else:
-        time = round(time, 2)
-    return time
+from docker import DockerClient
+import logger_config
+import logging
 
-def byte2Kb(mem):
-    mem = mem / 1024
-    # if using memory less than 1 Kb, don't take it into account
-    if mem < 1:
-        mem = None
-    else:
-        mem = round(mem, 2)
-    return mem
+logger = logging.getLogger(__name__)
+
+def start_docker_client():
+    try:
+        docker_client = DockerClient(base_url='unix://var/run/docker.sock')
+    except Exception:
+        logger.error(f'Exception has been caught during starting docker client.', exc_info=True)
+    return docker_client
